42c3ed9beff805e60b6d88aca1f0443558bf547d
erny-aisya <ernyaisya22@gmail.com>
2025-01-10 00:12:00 +0800

    Merge remote-tracking branch 'origin/master'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import TouristSpots from './pages/TouristSpots';
 import FoodBeverages from './pages/FoodBeverages';
